33e23f9bd0552140e34bffdd24ce9053dd3c6ee6
khanhdo05 <khanhphuongdo28@gmail.com>
2024-09-12 16:30:38 -0500

    initial commit

---
+++ src/main/java/edu/grinnell/csc207/App.java
@@ -0,0 +1,13 @@
+package edu.grinnell.csc207;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
